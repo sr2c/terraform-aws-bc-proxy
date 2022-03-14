d4de24944d2a576154916d5c81c55d3ccddc79a3
Darren Clarke <darren@redaranj.com>
2022-03-14 18:01:14 +0100

    First pass at bandwidth alerts

--- variables.tf
+++ variables.tf
@@ -8,3 +8,15 @@ variable "logging_bucket" {
   default = null
   description = "The domain name of an S3 bucket to store access logs."
 }
+
+variable "max_transfer_per_hour" {
+  type = string
+  default = "2147483648"
+  description = "The maximum number of bytes that can be sent out per hour."
+}
+
+variable "sns_topic_arn" {
+  type = string
+  default = null
+  description = "The ARN of an SNS topic to send notifications to."
+}
